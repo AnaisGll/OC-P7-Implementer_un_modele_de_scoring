6f730cc655eb7d9afc553101b6479d382a552da0
AnaisGll <168277083+AnaisGll@users.noreply.github.com>
2024-06-12 13:24:02 +0200

    Update api.py

--- api.py
+++ api.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
-import shap
 
 app = Flask(__name__)
 
@@ -13,9 +12,6 @@ pipeline = joblib.load('pipeline_LGBM_prediction.joblib')
 # Ajouter la colonne client_id
 train_data['client_id'] = range(1, len(train_data) + 1)
 test_data['client_id'] = range(1, len(test_data) + 1)
-
-# Initialiser l'explainer SHAP
-explainer = shap.Explainer(pipeline)
 
 @app.route('/')
 def home():
@@ -36,8 +32,8 @@ def check_client_id(client_id):
 @app.route('/prediction', methods=['POST'])
 def get_prediction():
     """
-    Calcule la probabilité de défaut pour un client et les valeurs SHAP.
-    :return: probabilité de défaut (float) et valeurs SHAP (dict).
+    Calcule la probabilité de défaut pour un client.
+    :return: probabilité de défaut (float).
     """
     data = request.get_json()
     client_id = data.get('client_id')
@@ -50,42 +46,11 @@ def get_prediction():
 
     info_client = client_data.drop('client_id', axis=1)
     prediction = pipeline.predict_proba(info_client)[0][1]
+    return jsonify({"prediction": prediction})
 
-    # Calculer les valeurs SHAP
-    shap_values = explainer(info_client)
-    shap_values_dict = dict(zip(info_client.columns, shap_values.values[0]))
 
-    return jsonify({"prediction": prediction, "shap_values": shap_values_dict})
-
-@app.route('/shaplocal/<int:client_id>', methods=['GET'])
-def shap_values_local(client_id):
-    """
-    Calcule les SHAP values pour un client.
-    :param: client_id (int)
-    :return: SHAP values du client (JSON).
-    """
-    client_data = test_data[test_data['client_id'] == client_id]
-    client_data = client_data.drop('client_id', axis=1)
-    shap_val = explainer.shap_values(client_data)[0][:, 1]
-
-    return jsonify({
-        'shap_values': shap_val.tolist(),
-        'base_value': shap_val.base_values,
-        'data': client_data.values.tolist(),
-        'feature_names': client_data.columns.tolist()
-    })
-
-@app.route('/shap/', methods=['GET'])
-def shap_values():
-    """
-    Calcule les SHAP values de l'ensemble du jeu de données.
-    :return: SHAP values (JSON).
-    """
-    shap_val = explainer.shap_values(test_data.drop('client_id', axis=1))
-    return jsonify({
-        'shap_values_0': shap_val[0].tolist(),
-        'shap_values_1': shap_val[1].tolist()
-    })
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, debug=True)
 
 
 if __name__ == '__main__':
